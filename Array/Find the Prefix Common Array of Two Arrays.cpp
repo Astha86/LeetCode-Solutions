9532478e8e2a2d8eb0352f81b5188c8a33784d8e
Astha Sahani <142304973+Astha86@users.noreply.github.com>
2025-01-14 23:56:14 +0530

    Added LeetCode 2657

---
+++ Array/Find the Prefix Common Array of Two Arrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        vector<int> v(n+1, 0);
+        vector<int> ans;
+        int x = 0;
+        
+        for (int i=0; i<n; i++) {
+            if (++v[A[i]] == 2) x++;
+            if (++v[B[i]] == 2) x++;
+            ans.push_back(x);
+        }
+        return ans;
+    }
+};
